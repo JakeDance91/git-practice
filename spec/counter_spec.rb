704da4f1e6befa7b523b012ddb252ae960aba416
William Marshall <w.marshall@elsevier.com>
2022-03-07 14:34:20 +0000

    Wrote and passed 2nd rspec test

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Counter do
     result = count.report
     expect(result).to eq "Counted to 7 so far."
   end
+
+  it "increases count to 2 user inputs" do
+    count = Counter.new
+    count.add(7)
+    count.add(3)
+    result = count.report
+    expect(result).to eq "Counted to 10 so far."
+  end
+
 end
